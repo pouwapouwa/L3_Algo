71fd35a11fb45ecb457feeaca5d699c4e2b14d2b
Amelie Guemon <amelie.guemon@gmail.com>
2015-04-14 16:55:05 +0200

    Pas sûre que ce soit mieux

--- tas/tas_min.c
+++ tas/tas_min.c
@@ -100,6 +100,23 @@ int heap_max(heap h){
   return node_max(h->root);
 }
 
+bool node_find_data(node n, int d){
+  if (n==NULL){
+    return false;}
+  else if (d == n->data){
+    return true;}
+  else if (d < n->data){
+    return false;
+  }
+  else{
+    return node_find_data(n->right, d) || node_find_data(n->left,d);
+  }
+}
+
+bool heap_find_data(heap h, int d){
+  return node_find_data(h->root, d);
+}
+
 int node_height(node n){
   if (n==NULL){
     return 0;}
@@ -111,27 +128,58 @@ int heap_height(heap h){
 }
 
 void swap(node a, node b){
-  node c = a;
-  b = a;
-  a = c;
+   int t =a->data;
+   a->data=b->data;
+   b->data=t;
 }
 
-void sift_up(node n){
+node node_merge(node n1, node n2){ //sans copie
+  if (n1 == NULL){
+    return n2;}
+  else if (n2 == NULL){
+    return n1;}
+  else{
+    if(n1->data < n2->data){
+      node t = n1->right;
+      n1->right=n2;
+      n2->left=node_merge(n2->left,t);
+      return n1;
+    }
+    else{ //n2->data < n1->data
+      node t = n2->left;
+      n2->left=n1;
+      n1->right=node_merge(n1->right,t);
+      return n2;
+    }
+  }
+}
+
+heap heap_merge(heap h1, heap h2){
+  heap h = create_heap();
+  h->root=node_merge(h1->root, h2->root);
+  h1->root=NULL;
+  h2->root=NULL;
+  destroy_heap(h1);
+  destroy_heap(h2);
+  return h;
+}
+
+node sift_up(node n){
   if (n==NULL){
-    return ;}
+    return n;}
   assert(node_height(n->left) >= node_height(n->right) && "La branche droite est plus lourde que la branche gauche");
   if (node_height(n->left) > node_height(n->right)){
     assert(!(node_height(n->left->left) < node_height(n->left->right)) && "Branche gauche->droite plus lourde que gauche->gauche");
-    sift_up(n->left);
+    n->left=sift_up(n->left);
     if (n->left!=NULL && n->left->data < n->data){
-      swap(n, n->left);}
-    return ;
+      swap(n->left, n);}
+    return n;
   }
   else{
-    sift_up(n->right);
+    n->right=sift_up(n->right);
     if (n->right!=NULL && n->right->data < n->data){
-      swap(n, n->right);}
-    return ;
+      swap(n->right, n);}
+    return n;
   }
 }
 
@@ -160,7 +208,27 @@ void heap_insert(heap h, int n){
     }
   else{
     node_insert(h->root, n);}
-  sift_up(h->root);
+  h->root=sift_up(h->root);
+}
+
+node node_delete(node n, int d){
+  if (n==NULL){
+    return NULL;}
+  else if (n->data == d){
+    node u=node_merge(n->right, n->left);
+    n->right=NULL;
+    n->left=NULL;
+    destroy_node(n);
+    return u;
+  }
+  else if (n->data < d){
+    if (node_find_data(n->right,d)){
+      return node_delete(n->right, d);}
+    else{
+      return node_delete(n->left, d);}
+  }
+  else {
+    return NULL;}
 }
 
 void print_node(node n){
@@ -180,33 +248,38 @@ void print_heap(heap h){
   print_node(h->root);
 }
 
+void heap_delete(heap h, int d){
+  if (is_empty(h)){
+    return ;
+  }
+  else {
+    h->root=node_delete(h->root,d);
+  }
+  print_heap(h);
+  h->root=sift_up(h->root);
+}
+
 int main(){
   printf("création du tas\n");
   heap h = create_heap();
   print_heap(h);
   printf("\nInsertion de 8, 12, 0, 14, 3, 1, 2\n");
   heap_insert(h, 8);
-  print_heap(h);
-  printf("\n");
   heap_insert(h, 12);
-  print_heap(h);
-  printf("\n");
-  heap_insert(h, 0);
-  print_heap(h);
-  printf("\n");
+  heap_insert(h, 0); //
   heap_insert(h, 14);
-  print_heap(h);
-  printf("\n");
   heap_insert(h, 3);
-  print_heap(h);
-  printf("\n");
   heap_insert(h, 1);
-  print_heap(h);
-  printf("\n");
   heap_insert(h, 2);
-
   printf("Affichage après ajout !\n");
   print_heap(h);
+
+  printf("Suppression de 2!\n");
+  heap_delete(h,2);
+  print_heap(h);
+
+
+  
   printf("\nDestrouctiounes ! \n");
   destroy_heap(h);
   return 0;
